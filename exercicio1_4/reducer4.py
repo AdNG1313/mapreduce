3100a30c55ac26b33716fafd03276131043ac275
AdNG1313 <adng1313@gmail.com>
2022-01-13 18:31:49 +0100

    Exercicio4 clavevalor

--- exercicio1_4/reducer4.py
+++ exercicio1_4/reducer4.py
@@ -6,11 +6,11 @@ ventaAlta = 0
 
 for line in sys.stdin:
     data_mapped = line.strip().split("\t")
-    if len(data_mapped) != 1:
+    if len(data_mapped) != 2:
         # Something has gone wrong. Skip this line.
         continue
 
-    dolarPago = data_mapped[0]
+    tipoPago, dolarPago = data_mapped
 
     if ventaAlta < float(dolarPago):
         ventaAlta = float(dolarPago)
