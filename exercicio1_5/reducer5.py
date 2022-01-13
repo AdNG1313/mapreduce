b7eb7e9e2300a1c6e9211768661a349589b879e7
AdNG1313 <adng1313@gmail.com>
2022-01-13 17:45:11 +0100

    Exercicio5

---
+++ exercicio1_5/reducer5.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys
+
+numeroVendas = 0
+totalVendas = 0
+
+for line in sys.stdin:
+    data_mapped = line.strip().split("\t")
+    if len(data_mapped) != 1:
+        # Something has gone wrong. Skip this line.
+        continue
+
+    dolarPago = data_mapped[0]
+
+    numeroVendas += 1
+    totalVendas += float(dolarPago)
+
+print("O ingreso total das ", numeroVendas," vendas foi: ", totalVendas)
+
